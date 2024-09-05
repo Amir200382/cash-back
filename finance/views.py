ef2c74c30fc065b81ec82404ec0f28a1f6fd4330
Amir200382 <a2003gholami@gmail.com>
2024-09-05 11:57:29 +0330

    view

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Transaction, SavingPlan, Budget
+from .serializers import TransactionSerializer, SavingPlanSerializer, BudgetSerializer
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class SavingPlanViewSet(viewsets.ModelViewSet):
+    queryset = SavingPlan.objects.all()
+    serializer_class = SavingPlanSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    queryset = Budget.objects.all()
+    serializer_class = BudgetSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
